76d2147285ea341d1cfa058a1182d87098434b99
DishantPoudel <dipshantpoudel@gmail.com>
2025-02-24 15:37:49 +0100

    Updated day1.py

--- day1.py
+++ day1.py
@@ -1,5 +1,21 @@
-print("Hello")
-a= 10
-b= 80
-sum=a+b
-print(sum)
+max_x=500
+max_y =500
+
+x=10
+y=10
+
+dx= 1
+dy= 1
+def draw_circle(x,y):
+    print((x,y))
+    
+while True:
+    x += dx
+    y +=dy
+    
+    if x > max_x or x <=0:
+        dx = -dx
+    if y > max_y or y <=0:
+        dy = -dy    
+    draw_circle(x,y)     
+    
